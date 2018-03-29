93f6f9ad154fd558ddb6775d6f6179d9dceb404d
巴厘岛 <32085514+zaizyp@users.noreply.github.com>
2018-03-29 16:48:53 +0800

    微信模块

--- application/weixin/controller/Index.php
+++ application/weixin/controller/Index.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\weixin\controller;
 
+use think\Request;
+
 class Index extends Base
 {
     public function index()
@@ -9,4 +11,28 @@ class Index extends Base
         return $this->fetch();
 
     }
+    public function test(){
+        if(input('nonce')&&input('timestamp')&&input('signature')){
+            $nonce     = input('nonce');
+            $token     = 'zyp123456';
+            $timestamp = input('timestamp');
+            $echostr   = input('echostr');
+            $signature = input('signature');
+            //形成数组，然后按字典序排序
+            $array = array();
+            $array = array($nonce, $timestamp, $token);
+            sort($array);
+            //拼接成字符串,sha1加密 ，然后与signature进行校验
+            $str = sha1( implode( $array ) );
+            if( $str == $signature ){
+                //第一次接入weixin api接口的时候
+                echo  $echostr;
+                exit;
+            }
+        }else{
+            echo "<p>验证失败</p>";
+        }
+
+    }
+
 }
