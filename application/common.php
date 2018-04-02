94452acaeb9527ab3342524c9987465a40df116b
My_PC <314617533@qq.com>
2018-04-02 16:37:46 +0800

    重新规划结构

--- application/common.php
+++ application/common.php
@@ -6,16 +6,8 @@
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: 流年 <liu21st@gmail.com>
+// | Author: 巴厘岛 <balidao@qq.com>
 // +----------------------------------------------------------------------
 
 // 应用公共文件
-function curl_file_get_contents($durl){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $durl);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $r = curl_exec($ch);
-    curl_close($ch);
-    return $r;
-}
+
---
+++ application/common/config.php
@@ -0,0 +1,6 @@
+<?php
+//配置文件
+return [
+    'appID'          =>     'wxc45ea54eb2e78e97',
+    'appsecret'     =>      '256cd76671c87ca1abdc70c4f0de152d',
+];
